8cfcb3ac521bf5c7ba000da8184fb88d76f3af27
Vyacheslav <stalker77726@gmail.com>
2017-06-18 15:41:24 +0500

    first commit

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model): #определение нашей модели, post является моделью Django
+    author = models.ForeignKey('auth.User')#ссылка на другую модель
+    title = models.CharField(max_length=200)#текстовое поле с ограниченым числом символо
+    text = models.TextField()#текстовое поле без ограничения длины символов
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)#дата и время
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title#возвращаем текст с заголовком записи
